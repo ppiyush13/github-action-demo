d505bf239da4a7f92d32769260658ad6f87fb024
ppiyush13 <ppiyush13@gmail.com>
2021-03-12 17:54:16 +0530

    specify npm token

--- scripts/tagger.js
+++ scripts/tagger.js
@@ -2,9 +2,6 @@ import shell from 'shelljs';
 import { getTagName } from './getTagName';
 
 (async () => {
-    console.log('Node version:', shell.exec(`node -v`));
-    console.log('NPM version:', shell.exec(`npm -v`));
-
     const distTagNames = await getTagName();
     const version = shell.exec(`node -p "require('./package.json').version"`).trim();
     const name = shell.exec(`node -p "require('./package.json').name"`).trim();
