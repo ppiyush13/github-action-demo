aadd198c089be1e017795b1cd1e4dcae64f3dbb3
ppiyush13 <ppiyush13@gmail.com>
2021-03-12 17:14:37 +0530

    Log version

--- scripts/tagger.js
+++ scripts/tagger.js
@@ -2,6 +2,9 @@ import shell from 'shelljs';
 import { getTagName } from './getTagName';
 
 (async () => {
+    console.log('Node version:', shell.exec(`node -v`));
+    console.log('NPM version:', shell.exec(`npm -v`));
+
     const distTagNames = await getTagName();
     const version = shell.exec(`node -p "require('./package.json').version"`);
     const name = shell.exec(`node -p "require('./package.json').name"`);
