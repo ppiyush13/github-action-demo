5fceda54940047646a32913c7df63e10f1c83a34
ppiyush13 <ppiyush13@gmail.com>
2021-04-01 21:14:06 +0530

    delete-taf

--- delete-tag.js
+++ delete-tag.js
@@ -2,12 +2,12 @@ const { Octokit } = require("@octokit/rest");
 
 /** check if tag mention */
 const tag = process.argv[2];
-if (!tag) return null;
+if (!tag) throw new Error('specify tag to be deleted');
 
 (async () => {
     /** create Octokit instance */
     const octokit = new Octokit({
-        auth: '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',
+        auth: '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',
     });
     const { data } = await octokit.rest.git.deleteRef({
         owner: 'ppiyush13',
